470f33625dc472b51551fdac5ecda346334641bd
chulbulji67 <61617202+chulbulji67@users.noreply.github.com>
2022-10-13 12:36:01 +0530

    Update Pizza.java

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -81,7 +81,7 @@ public class Pizza {
         if(isVeg) bill += "Base Price Of The Pizza: "+"300"+"\n";
         if(!isVeg)  bill+= "Base Price Of The Pizza: "+"400"+"\n";
         if(!extraCheese)  bill += "Extra Cheese Added: 80"+"\n";
-        if(!extraToppings) bill += "Extra Toppings Added: "extratopping+"\n";
+        if(!extraToppings) bill += "Extra Toppings Added: "+extratopping+"\n";
         if(!bag) bill += "Paperbag Added: 20"+"\n";
         if(this.getBill)bill += "Total Price: "+price;
              getBill = false;
